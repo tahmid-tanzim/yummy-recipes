8765b2a9e4641ff3968db78f03526ca32455cc0c
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-04-11 20:00:52 +0600

    29. Add validation for email field

--- main/core/models.py
+++ main/core/models.py
@@ -10,8 +10,11 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create & Save a New User"""
-        normalized_email = self.normalize_email(email)
-        user = self.model(email=normalized_email, **extra_fields)
+
+        if not email:
+            raise ValueError('Email address required')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -19,6 +22,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
--- main/core/tests/test_models.py
+++ main/core/tests/test_models.py
@@ -6,8 +6,9 @@ class ModelTestCase(TestCase):
 
     def test_create_user_with_email_successfull(self):
         """Ensure we can create a new user object."""
-        email = 'test@oneleven.com',
-        password = 'Te$t.123',
+
+        email = 'test@oneleven.com'
+        password = 'Te$t.123'
 
         user = get_user_model().objects.create_user(
             email=email,
@@ -19,11 +20,19 @@ class ModelTestCase(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test the email for a  new user is normalized"""
+
         email = 'test@ONELEVEN.COM'
+        password = 'Te$t.123'
 
         user = get_user_model().objects.create_user(
             email=email,
-            password='Te$t.123'
+            password=password
         )
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'Te$t.123')
