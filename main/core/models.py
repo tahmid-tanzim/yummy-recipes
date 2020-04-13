86e2ac8c0eb1c67490b274f2d9dcef7d50e44c92
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-04-13 18:18:00 +0600

    52. Add tag model

--- main/core/models.py
+++ main/core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin
 )
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -43,3 +44,45 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return "{0}<{1}>".format(self.name, self.email)
+
+
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+# class Ingredient(models.Model):
+#     """Ingredient to be used in a recipe"""
+#     name = models.CharField(max_length=255)
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Recipe(models.Model):
+#     """Recipe object"""
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#     title = models.CharField(max_length=255)
+#     time_minutes = models.IntegerField()
+#     price = models.DecimalField(max_digits=5, decimal_places=2)
+#     link = models.CharField(max_length=255, blank=True)
+#     ingredients = models.ManyToManyField('Ingredient')
+#     tags = models.ManyToManyField('Tag')
+#     image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+#
+#     def __str__(self):
+#         return self.title
