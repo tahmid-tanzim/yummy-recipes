9de222715d169a2c41391e223a49fdac95b3669c
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-04-14 01:03:02 +0600

    60. Re-factor tags and ingredients viewsets

--- main/recipe/views.py
+++ main/recipe/views.py
@@ -9,85 +9,42 @@ from core.models import Tag, Ingredient  # , Recipe
 from recipe import serializers
 
 
-# class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
-#                             mixins.ListModelMixin,
-#                             mixins.CreateModelMixin):
-#     """Base viewset for user owned recipe attributes"""
-#     authentication_classes = (TokenAuthentication,)
-#     permission_classes = (IsAuthenticated,)
-#
-#     def get_queryset(self):
-#         """Return objects for the current authenticated user only"""
-#         assigned_only = bool(
-#             int(self.request.query_params.get('assigned_only', 0))
-#         )
-#         queryset = self.queryset
-#         if assigned_only:
-#             queryset = queryset.filter(recipe__isnull=False)
-#
-#         return queryset.filter(
-#             user=self.request.user
-#         ).order_by('-name').distinct()
-#
-#     def perform_create(self, serializer):
-#         """Create a new object"""
-#         serializer.save(user=self.request.user)
-#
-#
-# class TagViewSet(BaseRecipeAttrViewSet):
-#     """Manage tags in the database"""
-#     queryset = Tag.objects.all()
-#     serializer_class = serializers.TagSerializer
-
-
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """Base viewset for user owned recipe attributes"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        # assigned_only = bool(
+        #     int(self.request.query_params.get('assigned_only', 0))
+        # )
+        # queryset = self.queryset
+        # if assigned_only:
+        #     queryset = queryset.filter(recipe__isnull=False)
+
+        return self.queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
+
+    def perform_create(self, serializer):
+        """Create a new object"""
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database"""
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
 
-        return self.queryset.filter(
-            user=self.request.user
-        ).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a new object"""
-        serializer.save(user=self.request.user)
-
-
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
-    """Manage tags in the database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database"""
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
 
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-
-        return self.queryset.filter(
-            user=self.request.user
-        ).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a new object"""
-        serializer.save(user=self.request.user)
-
-
-# class IngredientViewSet(BaseRecipeAttrViewSet):
-#     """Manage ingredients in the database"""
-#     queryset = Ingredient.objects.all()
-#     serializer_class = serializers.IngredientSerializer
-#
-#
 # class RecipeViewSet(viewsets.ModelViewSet):
 #     """Manage recipes in the database"""
 #     serializer_class = serializers.RecipeSerializer
